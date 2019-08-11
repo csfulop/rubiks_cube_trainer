ebcc0d1f0566b3082df1c6ff98e75a04b5a1af0d
Csaba Fülöp <csfulop@users.noreply.github.com>
2019-08-11 06:49:32 +0200

    generate question and draw question mark

--- rubik_blind.js
+++ rubik_blind.js
@@ -5,44 +5,56 @@ var Y = 10;
 var SIDE = 50;
 var BORDER = 5;
 
-var FACE_F = 1;
-var FACE_U = 2;
-var FACE_R = 3;
-var FACE_L = 4;
-var FACE_B = 5;
-var FACE_D = 6;
+var FACE_F = 0;
+var FACE_U = 1;
+var FACE_R = 2;
+var FACE_L = 3;
+var FACE_B = 4;
+var FACE_D = 5;
 
+FACE_U_X = X + 2 * SIDE + 1 * BORDER;
+FACE_U_Y = Y;
+FACE_D_X = X + 2 * SIDE + 1 * BORDER;
+FACE_D_Y = Y + 4 * SIDE + 2 * BORDER;
+FACE_F_X = X + 2 * SIDE + 1 * BORDER;
+FACE_F_Y = Y + 2 * SIDE + 1 * BORDER;
+FACE_B_X = X + 6 * SIDE + 3 * BORDER;
+FACE_B_Y = Y + 2 * SIDE + 1 * BORDER;
+FACE_L_X = X;
+FACE_L_Y = Y + 2 * SIDE + 1 * BORDER;
+FACE_R_X = X + 4 * SIDE + 2 * BORDER;
+FACE_R_Y = Y + 2 * SIDE + 1 * BORDER;
 
 var TRANSFORMS = {};
 var TRANSFORM_U_FACE = [
   [SIDE, 0],
   [0, SIDE],
-  [X + 2 * SIDE + 1 * BORDER, Y]
+  [FACE_U_X, FACE_U_Y]
 ];
 var TRANSFORM_D_FACE = [
   [SIDE, 0],
   [0, SIDE],
-  [X + 2 * SIDE + 1 * BORDER, Y + 4 * SIDE + 2 * BORDER]
+  [FACE_D_X, FACE_D_Y]
 ];
 var TRANSFORM_F_FACE = [
   [SIDE, 0],
   [0, SIDE],
-  [X + 2 * SIDE + 1 * BORDER, Y + 2 * SIDE + 1 * BORDER]
+  [FACE_F_X, FACE_F_Y]
 ];
 var TRANSFORM_B_FACE = [
   [SIDE, 0],
   [0, SIDE],
-  [X + 6 * SIDE + 3 * BORDER, Y + 2 * SIDE + 1 * BORDER]
+  [FACE_B_X, FACE_B_Y]
 ];
 var TRANSFORM_L_FACE = [
   [SIDE, 0],
   [0, SIDE],
-  [X, Y + 2 * SIDE + 1 * BORDER]
+  [FACE_L_X, FACE_L_Y]
 ];
 var TRANSFORM_R_FACE = [
   [SIDE, 0],
   [0, SIDE],
-  [X + 4 * SIDE + 2 * BORDER, Y + 2 * SIDE + 1 * BORDER]
+  [FACE_R_X, FACE_R_Y]
 ];
 var TRANSFORMS = {};
 TRANSFORMS[FACE_U] = TRANSFORM_U_FACE;
@@ -61,6 +73,8 @@ var YELLOW = 5;
 var GREY = 6;
 
 var COLORS = ["red", "orange", "blue", "green", "white", "yellow", "grey"];
+
+var QUESTION;
 
 function mul(vector, matrix) {
   vector.push(1);
@@ -103,7 +117,7 @@ function getRandomInt(max) {
   return Math.floor(Math.random() * (max - min)) + min;
 }
 
-function drawGreyCube() {
+function drawFlattenedCube() {
   for (var x = 0; x < 2; x++) {
     for (var y = 0; y < 2; y++) {
       drawCubicle(x, y, COLORS[YELLOW], FACE_U);
@@ -116,9 +130,25 @@ function drawGreyCube() {
   }
 }
 
+function moveQuestionmark() {
+  face = Math.floor(QUESTION / 4);
+  y = Math.floor((QUESTION % 4) / 2);
+  x = QUESTION % 2;
+  console.log("face=%d, x=%d, y=%d", face, x, y);
+  transform = TRANSFORMS[face];
+  v = [x, y];
+  v2 = mul(v, transform);
+  document.getElementById("questionmark").style.fontSize = SIDE;
+  document.getElementById("questionmark").style.left = v2[0] + SIDE / 4;
+  document.getElementById("questionmark").style.top = v2[1];
+  document.getElementById("questionmark").style.visibility = "visible";
+}
+
 function generateQuiz() {
   ctx.clearRect(0, 0, canvas.width, canvas.height);
-  drawGreyCube();
+  drawFlattenedCube();
+  QUESTION = getRandomInt(6 * 4);
+  moveQuestionmark();
 }
 
 generateQuiz();
